b3da7490b161589d58cbdc231ff362d36ff5275d
patrickNgare <patrickngare@gmail.com>
2018-10-07 23:44:27 +0300

    debuggig

--- Assignment/challenge/views.py
+++ Assignment/challenge/views.py
@@ -5,8 +5,18 @@ from django.shortcuts import render
 def index(request):
     
     url='http://api.worldbank.org/v2/datacatalog?format=json'
-    res=requests.get(url.format()).json()
-    print(res)
-    context={'res':res}
+    res=requests.get(url)
+    data=res.json()
+    print(data)
+    test = "Working"
+    dat_des={
+        'name':data['metatype']['name'],
+        
+
+    }
+    context={'dat_des':dat_des}
 
     return render(request,'all/index.html',context)
+
+
+        
